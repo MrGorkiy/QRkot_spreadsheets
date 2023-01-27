a7afe34e38c299abedaa48790c7582e7fa21e291
MrGorkiy <mbrysin@bk.ru>
2023-01-27 14:31:39 +0300

    UPD Исправляет замечания ревьюера

--- app/api/endpoints/google.py
+++ app/api/endpoints/google.py
@@ -13,6 +13,8 @@ from app.services.google_api import (set_user_permissions, spreadsheets_create,
 
 router = APIRouter()
 
+GOOGLE_SHEETS_URL = 'https://docs.google.com/spreadsheets/d/'
+
 
 @router.post(
     '/',
@@ -31,7 +33,7 @@ async def get_spreadsheet_report(
     projects = await charity_project_crud.get_projects_by_completion_rate(
         session
     )
-    spreadsheetid = await spreadsheets_create(aiogoogle_object)
-    await set_user_permissions(spreadsheetid, aiogoogle_object)
-    await spreadsheets_update_value(spreadsheetid, projects, aiogoogle_object)
-    return 'https://docs.google.com/spreadsheets/d/' + spreadsheetid
+    spreadsheet_id = await spreadsheets_create(aiogoogle_object)
+    await set_user_permissions(spreadsheet_id, aiogoogle_object)
+    await spreadsheets_update_value(spreadsheet_id, projects, aiogoogle_object)
+    return GOOGLE_SHEETS_URL + spreadsheet_id
--- app/services/google_api.py
+++ app/services/google_api.py
@@ -6,25 +6,46 @@ from app.core.config import settings
 from app.models import CharityProject
 
 FORMAT = "%Y/%m/%d %H:%M:%S"
+SPREADSHEET_ROWCOUNT_DRAFT = 100
+SPREADSHEET_COLUMNCOUNT_DRAFT = 11
+SPREADSHEET_BODY = dict(
+    properties=dict(
+        title='Отчет на ',
+        locale='ru_RU',
+    ),
+    sheets=[dict(properties=dict(
+        sheetType='GRID',
+        sheetId=0,
+        title='Лист1',
+        gridProperties=dict(
+            rowCount=SPREADSHEET_ROWCOUNT_DRAFT,
+            columnCount=SPREADSHEET_COLUMNCOUNT_DRAFT
+        )
+    ))]
+)
+TABLE_VALUES_DRAFT = [
+    ['Отчет от', ],
+    ['Топ проектов по скорости закрытия'],
+    ['Название проекта', 'Время сбора', 'Описание']
+]
+ROW_COLUMN_COUNT_TOO_BIG = ('В ваших данных строк - {rows_value}, а'
+                            'столбцов - {columns_value}, но'
+                            'количество строк не'
+                            'должно превышать {rowcount_draft}, '
+                            'a столбцов - {columncount_draft}')
 
 
 async def spreadsheets_create(wrapper_services: Aiogoogle) -> str:
     """Создаём документ."""
+    now_date_time = datetime.now().strftime(FORMAT)
     service = await wrapper_services.discover('sheets', 'v4')
-    spreadsheet_body = {
-        'properties': {'title': 'Отчет по закрытым проектам',
-                       'locale': 'ru_RU'},
-        'sheets': [{'properties': {'sheetType': 'GRID',
-                                   'sheetId': 0,
-                                   'title': 'Лист1',
-                                   'gridProperties': {'rowCount': 100,
-                                                      'columnCount': 11}}}]
-    }
+    spreadsheets_body = SPREADSHEET_BODY.copy()
+    spreadsheets_body['properties']['title'] += now_date_time
     response = await wrapper_services.as_service_account(
-        service.spreadsheets.create(json=spreadsheet_body)
+        service.spreadsheets.create(json=SPREADSHEET_BODY)
     )
-    spreadsheetid = response['spreadsheetId']
-    return spreadsheetid
+    spreadsheet_id = response['spreadsheetId']
+    return spreadsheet_id
 
 
 async def set_user_permissions(
@@ -52,26 +73,36 @@ async def spreadsheets_update_value(
     """Наполняем документ данными."""
     now_date_time = datetime.now().strftime(FORMAT)
     service = await wrapper_services.discover('sheets', 'v4')
-    table_values = [
-        ['Отчет от', now_date_time],
-        ['Топ проектов по скорости закрытия'],
-        ['Название проекта', 'Время сбора', 'Описание проекта']
-    ]
-    for project in charity_project:
-        table_values.append([
-            str(project.name),
-            str(project.close_date - project.create_date),
-            str(project.description)
-        ])
 
+    table_values = TABLE_VALUES_DRAFT.copy()
+    table_values[0].append(now_date_time)
+    table_values = [*table_values,
+                    *[list(map(str,
+                               [project.name,
+                                project.close_date - project.create_date,
+                                project.description])) for project in
+                      charity_project]
+                    ]
     update_body = {
         'majorDimension': 'ROWS',
         'values': table_values
     }
+
+    columns_value = max(len(items_to_count)
+                        for items_to_count in table_values)
+    rows_value = len(table_values)
+    if (SPREADSHEET_ROWCOUNT_DRAFT < rows_value or
+            SPREADSHEET_COLUMNCOUNT_DRAFT < columns_value):
+        raise ValueError(ROW_COLUMN_COUNT_TOO_BIG.format(
+            rows_value=rows_value,
+            columns_value=columns_value,
+            rowcount_draft=SPREADSHEET_ROWCOUNT_DRAFT,
+            columncount_draft=SPREADSHEET_COLUMNCOUNT_DRAFT))
+
     await wrapper_services.as_service_account(
         service.spreadsheets.values.update(
             spreadsheetId=spreadsheet_id,
-            range='A1:E30',
+            range=f'R1C1:R{rows_value}C{columns_value}',
             valueInputOption='USER_ENTERED',
             json=update_body
         )
